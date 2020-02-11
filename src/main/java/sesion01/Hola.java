aae02f4a5d340132a709131758e06ba9f68ccb06
Christopher <cmurillo.humbert@outlook.com>
2020-02-10 21:15:17 -0500

    Primer commit

---
+++ src/main/java/sesion01/Hola.java
@@ -0,0 +1,23 @@
+package sesion01;
+
+import java.util.Scanner;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Imprimir mensaje
+		System.out.println("Bienvenido Mitocoder");
+		
+		//Capturar valores de la consola
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Cual es tu nombre?");
+		
+		String nombre = sc.nextLine();
+		System.out.println("Bienvenido "+ nombre);
+		
+		sc.close();
+		
+	}
+
+}
